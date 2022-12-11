59b874c146f3e5af5f1d1d6fe19baf4061623736
ThomasBoxall <38958967+ThomasBoxall@users.noreply.github.com>
2022-12-11 20:13:20 +0000

    add calendar and clean up js

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # purefm-clock
-Customised digital clock for Pure FM at UoP
+
+Customised digital clock for [PureFM](https://thisispurefm.com).
+
+Written in HTML, CSS and JS.  
+
+This project is a work in progress. Updates will be made eventually.
+
+## To do list
+- Fix the scrolling on the Calendar Box
+- Style the calendar box
+- Add more pages with different view options (eg. a page without calendar)
--- script.js
+++ script.js
@@ -1,13 +1,10 @@
-function main(){
-    setCurrentTime();
-}
-
 function setCurrentTime(){
-    var currentDateTime = new Date()
+    const currentDateTime = new Date()
     setNumericTime(currentDateTime);
     setWrittenTime(currentDateTime);
     setDate(currentDateTime);
-    tickClock();
+    tickClock(currentDateTime);
+    checkReload(currentDateTime);
 }
 
 function setNumericTime(currentDateTime){
@@ -16,7 +13,7 @@ function setNumericTime(currentDateTime){
         minute: "numeric",
         second: "numeric",
     };
-    var timeToDisplay = currentDateTime.toLocaleString('en-GB', timeNowOptions);
+    let timeToDisplay = currentDateTime.toLocaleString('en-GB', timeNowOptions);
     document.getElementById("numeric-time").innerHTML = timeToDisplay;
 }
 
@@ -27,376 +24,49 @@ function setDate(currentDateTime){
         month: "long",
         year: "numeric",
     };
-    var dateToDisplay = currentDateTime.toLocaleString('en-GB', dateOptions);
+    let dateToDisplay = currentDateTime.toLocaleString('en-GB', dateOptions);
     document.getElementById("date").innerHTML = dateToDisplay;
 }
 
-
 function setWrittenTime(currentDateTime){
-    var pToSet = document.getElementById("words-time");
-    var timeToPrint = getMinute(currentDateTime) + " " + getHour(currentDateTime);
-    
-    pToSet.innerHTML = timeToPrint;
+    let mins = currentDateTime.getMinutes();
+    let hours = currentDateTime.getHours();
+    document.getElementById("words-time").innerHTML = getWrittenTime(hours, mins);
 }
 
-
-function getHour(currentTime){
-    var hourNumb = currentTime.getHours();
-    var hour;
-    switch (hourNumb) {
-        case 1:
-        case 13:
-            hour = "one"
-            break;
-        case 2:
-        case 14:
-            hour = "two"
-            break;
-        case 3:
-        case 15:
-            hour = "three"
-            break;
-        case 4:
-        case 16:
-            hour = "four"
-            break;
-        case 5:
-        case 17:
-            hour = "five"
-            break;
-        case 6:
-        case 18:
-            hour = "four"
-            break;
-        case 7:
-        case 19:
-            hour = "seven"
-            break;
-        case 8:
-        case 20:
-            hour = "eight"
-            break;
-        case 9:
-        case 21:
-            hour = "nine"
-            break;
-        case 10:
-        case 22:
-            hour = "ten"
-            break;
-        case 11:
-        case 23:
-            hour = "eleven"
-            break;
-        case 12:
-        case 0:
-            hour = "twelve"
-            break;
-        default:
-            break;
+const getWrittenTime = (h, m) => {
+    h = h % 12;
+    let count = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen", "twenty", "twenty one", "twen two", "twenty three", "twenty four", "twenty five", "twenty six", "twenty seven", "twenty eight", "twenty nine", "thirty"]
+    if (m === 1) {
+        return count[m] + " minute past " + count[h];
+    } else
+    if (m === 59) {
+        return count[60 - m] + " minute to " + count[h + 1];
+    } else
+    if (m === 0 ) {
+        return count[h] + " o'clock"; 
+    } else
+    if (m === 15) {
+        return "Quarter past " + count[h];
+    } else
+    if (m === 30) {
+        return "half past " + count[h];
+    } else 
+    if (m === 45) {
+        return "Quarter to " + count[h];
+    } else
+    if (m < 30) {
+        return count[m] + " minutes past " + count[h];
+    } else 
+    if (m > 30) {
+        return count[60 - m] + " minutes to " + count[h + 1];
     }
-    // code below handles when the time is on the hour as we need it to output [hour] + o'clock rather than just [hour] as there won't be a minute pre-fix on it.
-    var mins = currentTime.getMinutes();
-    var endingString = " ";
-    if (mins === 0){
-        // we need to return hour + " o'clock"
-        endingString = "o'clock";
-    }
-    return hour.concat(" ", endingString);
-}
+};
 
-function getMinute(currentTime){
-    var minuteNumb = currentTime.getMinutes();
-    var mins;
-    var ending;
-    switch (minuteNumb) {
-        case 1:
-            mins = "one";
-            ending = 1;
-            break;
-        case 2:
-            mins = "two";
-            ending = 2;
-            break;
-        case 3:
-            mins = "three";
-            ending = 2;
-            break;
-        case 4:
-            mins = "four";
-            ending = 2;
-            break;
-        case 5:
-            mins = "five";
-            ending = 2;
-            break;
-        case 5:
-            mins = "five";
-            ending = 2;
-            break;
-        case 6:
-            mins = "six";
-            ending = 2;
-            break;
-        case 7:
-            mins = "seven";
-            ending = 2;
-            break;
-        case 8:
-            mins = "eight";
-            ending = 2;
-            break;
-        case 9:
-            mins = "nine";
-            ending = 2;
-            break;
-        case 10:
-            mins = "ten";
-            ending = 2;
-            break;
-        case 11:
-            mins = "eleven";
-            ending = 2;
-            break;
-        case 12:
-            mins = "twelve";
-            ending = 2;
-            break;
-        case 13:
-            mins = "thirteen";
-            ending = 2;
-            break;
-        case 14:
-            mins = "fourteen";
-            ending = 2;
-            break;
-        case 15:
-            mins = "quarter";
-            ending = 3;
-            break;
-        case 16:
-            mins = "sixteen";
-            ending = 2;
-            break;
-        case 17:
-            mins = "seventeen";
-            ending = 2;
-            break;
-        case 18:
-            mins = "eighteen";
-            ending = 2;
-            break;
-        case 19:
-            mins = "nineteen";
-            ending = 2;
-            break;
-        case 20:
-            mins = "twenty";
-            ending = 2;
-            break;
-        case 21:
-            mins = "twenty one";
-            ending = 2;
-            break;
-        case 22:
-            mins = "twenty two";
-            ending = 2;
-            break;
-        case 23:
-            mins = "twenty three";
-            ending = 2;
-            break;
-        case 24:
-            mins = "twenty four";
-            ending = 2;
-            break;
-        case 25:
-            mins = "twenty five";
-            ending = 2;
-            break;
-        case 26:
-            mins = "twenty six";
-            ending = 2;
-            break;
-        case 27:
-            mins = "twenty seven";
-            ending = 2;
-            break;
-        case 28:
-            mins = "twenty eight";
-            ending = 2;
-            break;
-        case 29:
-            mins = "twenty nine";
-            ending = 2;
-            break;
-        case 30:
-            mins = "half";
-            ending = 3;
-            break;
-        case 31:
-            mins = "thirty one";
-            ending = 2;
-            break;
-        case 32:
-            mins = "thirty two";
-            ending = 2;
-            break;
-        case 33:
-            mins = "thirty three";
-            ending = 2;
-            break;
-        case 34:
-            mins = "thirty four";
-            ending = 2;
-            break;
-        case 35:
-            mins = "thirty five";
-            ending = 2;
-            break;
-        case 36:
-            mins = "thirty six";
-            ending = 2;
-            break;
-        case 37:
-            mins = "thirty seven";
-            ending = 2;
-            break;
-        case 38:
-            mins = "thirty eight";
-            ending = 2;
-            break;
-        case 39:
-            mins = "thirty nine";
-            ending = 2;
-            break;
-        case 40:
-            mins = "twenty";
-            ending = 4;
-            break;
-        case 41:
-            mins = "forty one";
-            ending = 2;
-            break;
-        case 42:
-            mins = "forty two";
-            ending = 2;
-            break;
-        case 43:
-            mins = "forty three";
-            ending = 2;
-            break;
-        case 44:
-            mins = "forty four";
-            ending = 2;
-            break;
-        case 45:
-            mins = "quarter";
-            ending = 5;
-            break;
-        case 46:
-            mins = "forty six";
-            ending = 2;
-            break;
-        case 47:
-            mins = "forty seven";
-            ending = 2;
-            break;
-        case 48:
-            mins = "forty eight";
-            ending = 2;
-            break;
-        case 49:
-            mins = "forty nine";
-            ending = 2;
-            break;
-        case 50:
-            mins = "ten";
-            ending = 4;
-            break;
-        case 51:
-            mins = "fifty one";
-            ending = 2;
-            break;
-        case 52:
-            mins = "fifty two";
-            ending = 2;
-            break;
-        case 53:
-            mins = "fifty three";
-            ending = 2;
-            break;
-        case 54:
-            mins = "fifty four";
-            ending = 2;
-            break;
-        case 55:
-            mins = "five";
-            ending = 4;
-            break;
-        case 56:
-            mins = "four";
-            ending = 4;
-            break;
-        case 57:
-            mins = "three";
-            ending = 4;
-            break;
-        case 58:
-            mins = "two";
-            ending = 4;
-            break;
-        case 59:
-            mins = "one";
-            ending = 6;
-            break;
-        case 0:
-            // we don't want to add a minute on here as the time should read "[hour] o'clock"
-            mins = "";
-            ending = 0;
-            break;
-        default:
-            mins = "undefined;"
-            ending = 0;
-            break;
-    }
-    var endingText;
-    switch(ending){
-        case 0:
-            endingText = " ";
-            break;
-        case 1:
-            endingText = "minute past";
-            break;
-        case 2:
-            endingText = "minutes past";
-            break;
-        case 3:
-            endingText = "past";
-            break;
-        case 4:
-            endingText = "minutes to";
-            break;
-        case 5:
-            endingText = "to";
-            break;
-        case 6:
-            endingText = "minute to";
-            break;
-        default:
-            endingText = "undefined";
-            break;
-    }
-    var retString = mins.concat(" ", endingText);
-    return retString;
-}
-
-function tickClock() {
-    var secondHand = document.getElementById('second-hand');
-    var minsHand = document.getElementById('min-hand');
-    var hourHand = document.getElementById('hour-hand');
-
-    const now = new Date();
+function tickClock(now) {
+    let secondHand = document.getElementById('second-hand');
+    let minsHand = document.getElementById('min-hand');
+    let hourHand = document.getElementById('hour-hand');
 
     const seconds = now.getSeconds();
     const secondsDegrees = ((seconds / 60) * 360) + 90;
@@ -411,6 +81,11 @@ function tickClock() {
     hourHand.style.transform = "rotate(" + hourDegrees + "deg)";
 }
 
+function checkReload(currentDateTime){
+    if (currentDateTime.getSeconds() == 24 && currentDateTime.getMinutes() % 5 == 0){
+        window.location.reload();
+    }
+}
 
 setInterval(setCurrentTime, 1000);
-main()
+setCurrentTime()
